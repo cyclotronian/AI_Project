f3eaf36d0c598d7f174c9a28fd8405e1f4ff931f
mohit singh <mdsingh2013@gmail.com>
2017-12-02 22:07:40 -0500

    adding twitter dataset

---
+++ Dataset/twitter_dataset/construct.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mkdir -p twitter_dialog
+
+cp test.utt twitter_dialog/test.utt
+cp test.resp twitter_dialog/test.resp
+cp train.utt twitter_dialog/train.utt
+cp train.resp twitter_dialog/train.resp
+cp dev.utt twitter_dialog/dev.utt
+cp dev.resp twitter_dialog/dev.resp
+cp vocab twitter_dialog/vocab.utt
+cp vocab twitter_dialog/vocab.resp
